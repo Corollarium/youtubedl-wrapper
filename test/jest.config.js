9b103b2f4a2c240c4b4d3185eec49dcc167d618c
Bruno <brunobg@gmail.com>
2020-08-20 12:02:40 -0300

    jest config

--- test/jest.config.js
+++ test/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   // setupTestFrameworkScriptFile has been deprecated in
   // favor of setupFilesAfterEnv in jest 24
-  setupFilesAfterEnv: ['./jest.setup.js']
-}
+  setupFilesAfterEnv: ["./jest.setup.js"]
+};
