45e4f13c5205df4b283fef9671c36a637cfda278
Bruno <brunobg@gmail.com>
2020-08-27 17:04:42 -0300

    fix regex

--- src/youtubedl.js
+++ src/youtubedl.js
@@ -67,7 +67,7 @@ class Youtubedl {
 
     function parseLine(line) {
       if (line.indexOf("[download]") >= 0) {
-        const regex = /\[download\]\s+(?<progress>[0-9.]+)%\s+of\s+~?(?<downloaded>[0-9.]+)(?<downloadedUnit>[a-zA-Z]+)\s+at\s+(?<speed>[0-9.%]+)(?<speedUnit>[a-zA-Z/]+)\s+ETA\s+(?<ETA>[0-9:]+)/;
+        const regex = /\[download\]\s+(?<progress>[0-9.]+)%\s+of\s+~?(?<download>[0-9.]+)(?<downloadUnit>[a-zA-Z]+)\s+at\s+(?<speed>[0-9a-zA-Z.%]+)\s*(?<speedUnit>[a-zA-Z/]+)\s+ETA\s+(?<ETA>[0-9:]+)/;
         const match = regex.exec(line);
         if (match) {
           yemit.emit("download", { ...match.groups, filename });
